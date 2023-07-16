3d865cd6b94bda15573d76847f6ddd3c4a08132d
sdutta24 <81972601+sdutta24@users.noreply.github.com>
2023-07-16 06:23:27 +0530

    Update streamlit_app.py
    Added more breakfast menu with icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard Boiled Free Range Eggs')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free Range Eggs')
+streamlit.text('🥑🍞 Avocado Toast')
