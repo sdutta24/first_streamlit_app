003422810fa0d09170cd983f3a51b906a878b420
sdutta24 <81972601+sdutta24@users.noreply.github.com>
2023-07-20 12:00:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+import urllib.error from URLError
 
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast Favorites')
@@ -22,6 +23,7 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_cho
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
