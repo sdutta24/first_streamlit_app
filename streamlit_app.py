acdc8e14298d983bfa42429b543dde513f56196a
sdutta24 <81972601+sdutta24@users.noreply.github.com>
2023-07-20 11:24:49 +0530

    Update streamlit_app.py
    added secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_cho
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-
-
-
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur=my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(), current_region()")
+my_data_row=my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
