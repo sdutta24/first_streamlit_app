f73d0a277074aca3a75fac350c245fc7f9afafd6
sdutta24 <81972601+sdutta24@users.noreply.github.com>
2023-08-09 13:36:17 +0530

    Update streamlit_app.py
    update11

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ df=pandas.DataFrame(my_catalog)
 streamlit.write(df)
 # put the first column into a list
 color_list=df[0].values.tolist()
+print(color_list)
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
