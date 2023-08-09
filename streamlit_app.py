3178aaf5ca074e4ab8c11c2ff56eaff96fdff272
sdutta24 <81972601+sdutta24@users.noreply.github.com>
2023-08-09 14:35:09 +0530

    Update streamlit_app.py
    update14

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,19 @@ df=pandas.DataFrame(my_catalog)
 streamlit.write(df)
 # put the first column into a list
 color_list=df[0].values.tolist()
-print(color_list)
+#print(color_list)
 # Let's put a pick list here so they can pick the color
 option=streamlit.selectbox('Pick a sweatsuit color or style',list(color_list))
+# We'll build the image caption now, since we can
+product_caption='Our warm comfortable '+option+ ' sweatsuit!'
+# use the option selected to go back and get all the info from the database
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
+color_or_style = '"+option+"';")
+df2=my_cur.fetchone()
+streamlit.image(df2,width=400,caption=product_caption)
+streamlit.write('Price: ',df2[1])
+streamlit.write('Sizes Available: ',df2[2])
+streamlit.write(df2[3])
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
